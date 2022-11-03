4c827bd3ff6db2f483d29f1b86975982b5fbd623
greatb1n <86897696+greatb1n@users.noreply.github.com>
2022-11-03 23:15:18 +0900

    Update register_action.php

--- register_action.php
+++ register_action.php
@@ -1,4 +1,5 @@
 <?php
+header("Content-Type:text/html;charset=utf-8");
 $connect = mysqli_connect('127.0.0.1', 'root', '1234', 'cykordb');
 
 $id = $_POST['id'];
@@ -11,7 +12,7 @@ $query1 = "select * from member where id='$id'";
 $result1 = $connect->query($query1);
 $exist = mysqli_num_rows($result1);
 
-if ($exist) {      //만약 중복된 id가 있다면
+if ($exist) {      
     ?>
     <script>
         alert('이미 존재하는 ID입니다.');
@@ -28,7 +29,7 @@ else if($pw != $pw_confirm) {
     <?php 
 }
 else {
-    $query = "insert into member(id, password, date, permit) values('$id', '$pw', '$date', 0)";
+    $query = "insert into member(id, password, date) values('$id', '$pw', '$date')";
 
     $result = $connect->query($query);
 
